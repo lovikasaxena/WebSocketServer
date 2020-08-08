95a1bb113ca37b88655608aa11d68c187aca857f
Lovika Saxena <slovika@thoughtworks.com>
2020-08-09 01:33:47 +0530

    [Lovika] - 1. Send notification to all clients about new user 2. Send new user list of all existing player user names

--- index.js
+++ index.js
@@ -11,26 +11,49 @@ const wsServer = new webSocketServer({
 })
 
 const clients = {}
-
+const clientUserNames = []
 function getUniqueID() {
-    return Math.random()
+    return (Math.random()*10000000000000000).toString(15)
 }
 
-wsServer.on('request', function(request) {
+function sendMessageToAll(clients, message) {
+    for (key in clients) {
+        clients[key].sendUTF(message.utf8Data)
+    }
+    return key;
+}
+
+function sendExistingPlayerUserNamesToNewUser(clientUserNames, connection) {
+    if(clientUserNames.length < 1) return
+
+    const notification = JSON.stringify({
+        type: "allPlayers",
+        playerUserNames: clientUserNames,
+    })
+    console.log('Sending all player notification: ', notification)
+    connection.send(notification)
+}
+
+wsServer.on('request', function (request) {
     var userID = getUniqueID();
     console.log((new Date()) + 'Received a new connection from origin' + request.origin + '.')
 
-    const connection  = request.accept(null, request.origin)
-    clients[userID] = connection;
+    const connection = request.accept(null, request.origin)
+    clients[userID] = connection
     console.log('Connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
 
-    connection.on('message', function(message) {
-        if(message.type === 'utf8') {
-            console.log('Recevied message', message.utf8Data)
-            for(key in clients) {
-                clients[key].sendUTF(message.utf8Data)
-                console.log('Sent message to', clients[key])
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') {
+            let utf8Message = JSON.parse(message.utf8Data);
+            console.log('Recevied message', utf8Message)
+
+            if (utf8Message.type === 'newPlayer') {
+                console.log('Adding new player to clients list: ', utf8Message)
+                sendExistingPlayerUserNamesToNewUser(clientUserNames, connection)
+                clientUserNames.push(utf8Message.userName)
             }
+
+            sendMessageToAll(clients, message)
         }
     })
 })
